965be774bfa8aae1568ce735a0fc3bf938965fa2
O-BaianoOficial007 <arthurandradepereira7@gmail.com>
2026-08-19 12:39:15 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Diferenciais from "./components/Diferencias";
 
 function App(){
   return(
     <>
       <Header/>
       <Hero/>
+      <Diferenciais/>
     </>
   )
 }
