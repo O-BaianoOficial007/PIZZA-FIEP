f690e5d9fbbf54e5aae5887c348d5a37b8216d33
O-BaianoOficial007 <arthurandradepereira7@gmail.com>
2026-08-20 11:17:45 -0300

    Add files via upload

---
+++ src/components/Cardapio.jsx
@@ -0,0 +1,19 @@
+export default function Cardapio(){
+    return(
+        <section>
+            <h2>Cardápio</h2>
+            <ul data-title="🍕 Tradicionais">
+                <li>Mussarela</li>
+                <li>Calabresa</li>
+                <li>Portuguesa</li>
+                <li>Marguerita</li>
+                <li>Frango com Catupiry</li>
+                <li>Quatro Queijos</li>
+                <li>Napolitana</li>
+                <li>Presunto e Queijo</li>
+                <li>Bacon</li>
+                <li>Milho com Bacon</li>
+            </ul>
+        </section>
+    )
+}
